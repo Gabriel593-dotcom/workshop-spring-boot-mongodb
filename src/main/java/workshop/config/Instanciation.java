3ec6d712d576a621c5f269794a0259496ffb3bba
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-01-10 20:37:40 -0300

    Instanciação de users no banco de dados

---
+++ src/main/java/workshop/config/Instanciation.java
@@ -0,0 +1,29 @@
+package workshop.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import workshop.domain.User;
+import workshop.repository.UserRepository;
+
+@Configuration
+public class Instanciation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User maria = new User(null, "Maria Souza", "maria@teste.com");
+		User alex = new User(null, "Alex Ferreira", "alex@teste.com");
+		User roberto = new User(null, "Roberto Nogueira", "roberto@teste.com");
+		
+		userRepository.deleteAll();
+		userRepository.saveAll(Arrays.asList(maria, alex, roberto));
+	}
+
+}
