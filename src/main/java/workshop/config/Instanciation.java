0fd88c743eda8e0877f12eef8564c55be8d7b5e9
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-01-11 20:21:52 -0300

    criando UserDTO e adequando UserResource

--- src/main/java/workshop/config/Instanciation.java
+++ src/main/java/workshop/config/Instanciation.java
@@ -10,18 +10,18 @@ import workshop.domain.User;
 import workshop.repository.UserRepository;
 
 @Configuration
-public class Instanciation implements CommandLineRunner{
+public class Instanciation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		User maria = new User(null, "Maria Souza", "maria@teste.com");
 		User alex = new User(null, "Alex Ferreira", "alex@teste.com");
 		User roberto = new User(null, "Roberto Nogueira", "roberto@teste.com");
-		
+
 		userRepository.deleteAll();
 		userRepository.saveAll(Arrays.asList(maria, alex, roberto));
 	}
