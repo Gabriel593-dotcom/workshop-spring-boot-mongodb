3a92ace8304828326e40a94ff39adde3efd50062
Gabriel593-dotcom <g.dos.pinheiro1999@gmail.com>
2022-01-13 20:59:32 -0300

    Projeção dos dados do Autor com DTO

--- src/main/java/workshop/config/Instanciation.java
+++ src/main/java/workshop/config/Instanciation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import workshop.domain.Post;
 import workshop.domain.User;
+import workshop.dto.AuthorDTO;
 import workshop.repository.PostRepository;
 import workshop.repository.UserRepository;
 
@@ -34,11 +35,11 @@ public class Instanciation implements CommandLineRunner {
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem.", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, roberto));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem.", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
